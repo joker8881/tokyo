2e1c2f39ae8b162657613092f7399a59459967f2
Tom <joker302876@gmail.com>
2023-11-23 15:52:52 +0800

    地圖與抓取方法更新，參考homeview

--- src/stores/abc.js
+++ src/stores/abc.js
@@ -9,7 +9,7 @@ export default defineStore("data",{
         obja: [],
         objs: [],
         objb: [],
-        objc: []
+        objc: [],
         //請在這裡加入新的陣列更應存取，不要使用重複的陣列，例如新資料b就使用陣列 objb
     }
     },
@@ -46,31 +46,16 @@ export default defineStore("data",{
             this.objs =data
             })
         },
-        // ck(x){
-        //     cc = x.getAttribute('id')
-        //     console.log(cc)
-        //     let obj1 = this.obja.records.location.filter(function(x, index, array){
-        //       return x.locationName == cc;
-        //     });
-        //   console.log(obj1[0].locationName)
-        //   console.log(obj1[0])
-        //   a.innerText = obj1[0].locationName
-        //     //=================這是找縣市名稱======================
-        //     console.log(obj1[0].weatherElement[0].time[cityTime].parameter.parameterName)
-        //     d.innerText = obj1[0].weatherElement[0].time[cityTime].parameter.parameterName
-        //     //=================這是天氣型態======================
-        //     console.log(obj1[0].weatherElement[1].time[cityTime].parameter.parameterName)
-        //     e.innerText = "降雨機率 " + obj1[0].weatherElement[1].time[cityTime].parameter.parameterName + " %"
-        //     //=================這是降雨機率======================
-        //     console.log(obj1[0].weatherElement[2].time[cityTime].parameter.parameterName)
-        //     b.innerText = "最低溫度 " + obj1[0].weatherElement[2].time[cityTime].parameter.parameterName + " ℃"
-        //     //=================這是最低溫度======================
-        //     console.log(obj1[0].weatherElement[4].time[cityTime].parameter.parameterName)
-        //     c.innerText = "最高溫度 " + obj1[0].weatherElement[4].time[cityTime].parameter.parameterName + " ℃"
-        //     //=================這是最高溫度======================
-        // }
-          //要怎麼只透過點擊物件就取得物件的名稱(onclick="ck(this);"使用getAttribute)。
-        //請在這邊加入新的資料調取方法，比如說公共設施 getPublic(){}
+        ck(){
+            const idarr = document.querySelectorAll("path[name='area']")
+            // console.dir(idarr)
+            idarr.forEach(item=>{
+                item.addEventListener("click",()=>{
+                    // console.dir(item)
+                    console.log(item.id)
+                })
+            })
+        }
     }
 
 })
