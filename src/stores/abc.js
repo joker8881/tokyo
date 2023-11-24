dae7e311ae679a14e382be7f5bf0883a17563f3b
Tom <joker302876@gmail.com>
2023-11-24 17:50:42 +0800

    天氣方法，除錯版本不要使用

--- src/stores/abc.js
+++ src/stores/abc.js
@@ -6,11 +6,35 @@ export default defineStore("data",{
 //與.vue中 script 預設匯出概念相同，只是名稱不同data
     state(){
         return{
-        obja: [],
         objs: [],
         objb: [],
         objc: [],
+//以下是天氣資料用的
+        objw1:[], //千代田區
+        objw2:[], //練馬區
+        objw3:[], 
+        objw4:[],
+        objw5:[],
+        objw6:[],
+        objw7:[],
+        objw8:[],
+        objw9:[],
+        objw10:[],
+        objw11:[],
+        objw12:[],
+        objw13:[],
+        objw14:[],
+        objw15:[],
+        objw16:[],
+        objw17:[],
+        objw18:[],
+        objw19:[],
+        objw20:[],
+        objw21:[],
+        objw22:[],
+        objw23:[],
         //請在這裡加入新的陣列更應存取，不要使用重複的陣列，例如新資料b就使用陣列 objb
+        x:"",
     }
     },
 
@@ -19,14 +43,27 @@ export default defineStore("data",{
 
     actions:{
         getWeather(){
-            fetch("https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-7B4DB2AF-A0C8-4401-BCFC-9D2BBBF12EDC")
+            fetch("https://api.openweathermap.org/data/2.5/weather?lat=35.684884&lon=139.753708&appid=59c6fd5a18b14258d6f6736a73fe8460&lang=zh_tw&units=metric")
             .then(response => response.json())
             .then(data => {
-            this.obja =data
+            this.objw1 =data
             })
+            //千代田區
+            fetch("https://api.openweathermap.org/data/2.5/weather?lat=35.737289&lon=139.653920&appid=59c6fd5a18b14258d6f6736a73fe8460&lang=zh_tw&units=metric")
+            .then(response => response.json())
+            .then(data => {
+            this.objw2 =data
+            })
+            //練馬區
+            fetch("https://api.data.metro.tokyo.lg.jp/v1/PublicFacility?limit=1000")
+            .then(response => response.json())
+            .then(data => {
+            this.objw3 =data
+            })
+            // //練馬區
         },
         getPublic(){
-            fetch("")
+            fetch("https://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&appid=59c6fd5a18b14258d6f6736a73fe8460&lang=zh_tw&units=metric")
             .then(response => response.json())
             .then(data => {
             this.objc =data
@@ -53,9 +90,10 @@ export default defineStore("data",{
                 item.addEventListener("click",()=>{
                     // console.dir(item)
                     console.log(item.id)
+                    this.x = item.id
                 })
             })
-        }
+        },
     }
 
 })
