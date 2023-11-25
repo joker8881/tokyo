3f567a05b1b3a4719460552c0080909d933f6c33
通報した <peterjackson0107>
2023-11-25 20:22:24 +0800

    文化第2版

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import './assets/main.scss'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+
+// 引入 Bootstrap 的 CSS 樣式表
+import './assets/main.scss' //初始化工作環境
+import "bootstrap"
 
 const app = createApp(App)
 
 const pinia = createPinia()
 
 app.use(router)
-
 app.use(pinia)
 
 app.mount('#app')
--- src/stores/abc.js
+++ src/stores/abc.js
@@ -1,76 +1,71 @@
 import { defineStore } from "pinia";
 
-export default defineStore("data",{
-//需要輸入兩個參數，1自建資料庫名稱，2資料庫內容
+export default defineStore("data", {
+    //需要輸入兩個參數，1自建資料庫名稱，2資料庫內容
 
-//與.vue中 script 預設匯出概念相同，只是名稱不同data
-    state(){
-        return{
-        obja: [],
-        objs: [],
-        objb: [],
-        objc: []
-        //請在這裡加入新的陣列更應存取，不要使用重複的陣列，例如新資料b就使用陣列 objb
-    }
+    //與.vue中 script 預設匯出概念相同，只是名稱不同data
+    state() {
+        return {
+            obja: [],
+            objs: [],
+            objb: [],
+            objc: [],
+            //請在這裡加入新的陣列更應存取，不要使用重複的陣列，例如新資料b就使用陣列 objb
+            x:"",
+        }
     },
 
-    getters:{
+    getters: {
     },
 
-    actions:{
-        getWeather(){
+    actions: {
+        getWeather() {
             fetch("https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-7B4DB2AF-A0C8-4401-BCFC-9D2BBBF12EDC")
-            .then(response => response.json())
-            .then(data => {
-            this.obja =data
-            })
+                .then(response => response.json())
+                .then(data => {
+                    this.obja = data
+                })
         },
-        getPublic(){
+        getPublic() {
             fetch("")
-            .then(response => response.json())
-            .then(data => {
-            this.objc =data
-            })
+                .then(response => response.json())
+                .then(data => {
+                    this.objc = data
+                })
         },
-        getWenhua(){
-            fetch("https://api.data.metro.tokyo.lg.jp/v1/CulturalProperty?limit=1000")
-            .then(response => response.json())
-            .then(data => {
-            this.objb =data
-            })
+        getWenhua() {
+            fetch("./bunnka/bunnka.json")
+                .then(response => response.json())
+                .then(data => {
+                    this.objb = data
+                    console.log(data)
+                })
         },
-        getSport(){
+        // fetch("https://api.data.metro.tokyo.lg.jp/v1/CulturalProperty?limit=1000")
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         this.objb = data
+        //         console.log(data)
+        //     })
+        // },
+        getSport() {
             fetch("")
-            .then(response => response.json())
-            .then(data => {
-            this.objs =data
-            })
+                .then(response => response.json())
+                .then(data => {
+                    this.objs = data
+                })
         },
-        // ck(x){
-        //     cc = x.getAttribute('id')
-        //     console.log(cc)
-        //     let obj1 = this.obja.records.location.filter(function(x, index, array){
-        //       return x.locationName == cc;
-        //     });
-        //   console.log(obj1[0].locationName)
-        //   console.log(obj1[0])
-        //   a.innerText = obj1[0].locationName
-        //     //=================這是找縣市名稱======================
-        //     console.log(obj1[0].weatherElement[0].time[cityTime].parameter.parameterName)
-        //     d.innerText = obj1[0].weatherElement[0].time[cityTime].parameter.parameterName
-        //     //=================這是天氣型態======================
-        //     console.log(obj1[0].weatherElement[1].time[cityTime].parameter.parameterName)
-        //     e.innerText = "降雨機率 " + obj1[0].weatherElement[1].time[cityTime].parameter.parameterName + " %"
-        //     //=================這是降雨機率======================
-        //     console.log(obj1[0].weatherElement[2].time[cityTime].parameter.parameterName)
-        //     b.innerText = "最低溫度 " + obj1[0].weatherElement[2].time[cityTime].parameter.parameterName + " ℃"
-        //     //=================這是最低溫度======================
-        //     console.log(obj1[0].weatherElement[4].time[cityTime].parameter.parameterName)
-        //     c.innerText = "最高溫度 " + obj1[0].weatherElement[4].time[cityTime].parameter.parameterName + " ℃"
-        //     //=================這是最高溫度======================
-        // }
-          //要怎麼只透過點擊物件就取得物件的名稱(onclick="ck(this);"使用getAttribute)。
-        //請在這邊加入新的資料調取方法，比如說公共設施 getPublic(){}
+        ck() {
+            const idarr = document.querySelectorAll("path[name='area']")
+            // console.dir(idarr)
+            idarr.forEach(item => {
+                item.addEventListener("click", () => {
+                    // console.dir(item)
+                    console.log(item.id)
+                    this.x = item.id
+                })
+            })
+        }
     }
 
 })
