9ecf5505f7819748a030b5b6904e7dd44f3c1248
通報した <peterjackson0107>
2023-11-27 23:04:15 +0800

    文化第4版結束......吧

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+// bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap"
+
+import * as echarts from 'echarts';
 
 const app = createApp(App)
 
